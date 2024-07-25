38d388da57fbd5cc7461cc3e1259b9c5d0ac789a
Mohan Drey <mohantampon@gmail.com>
2024-07-25 17:38:23 +0800

    lint fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { JobModule} from './jobs/job.module';
+import { JobModule } from './jobs/job.module';
 
 @Module({
   imports: [JobModule],
