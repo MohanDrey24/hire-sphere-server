394f6b429adfe84584d3a6d9a326dbe980d8f759
Mohan Drey <mohantampon@gmail.com>
2024-07-25 17:28:07 +0800

    update job service

--- src/jobs/job.service.ts
+++ src/jobs/job.service.ts
@@ -1,22 +1,22 @@
-import { Injectable, ConflictException } from '@nestjs/common'
-import { Job } from './interfaces/job.interface'
+import { Injectable, ConflictException } from '@nestjs/common';
+import { Job } from './interfaces/job.interface';
 
 @Injectable()
 export class JobService {
-  private readonly jobs: Job[] = []
+  private readonly jobs: Job[] = [];
 
   create(job: Job): Job {
-    const existingJob = this.jobs.find(elem => elem.id === job.id)
+    const existingJob = this.jobs.find((elem) => elem.id === job.id);
     if (existingJob) {
-      throw new ConflictException(`Job ID with ID ${job.id} already exists`)
+      throw new ConflictException(`Job ID with ID ${job.id} already exists`);
     }
 
-    this.jobs.push(job)
-    console.log(this.jobs)
-    return job
+    this.jobs.push(job);
+    console.log(this.jobs);
+    return job;
   }
 
   find(id: string): Job | undefined {
-    return this.jobs.find(job => job.id === id)
+    return this.jobs.find((job) => job.id === id);
   }
 }
