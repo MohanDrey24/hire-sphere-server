0a7441ee2bb6c0288888e6dabd97ecf1c19d0ca7
Mohan Drey <mohantampon@gmail.com>
2024-07-25 17:38:00 +0800

    update job controller

--- src/jobs/job.controller.ts
+++ src/jobs/job.controller.ts
@@ -1,24 +1,27 @@
 import { Controller, Get, Post, Body, Param, HttpStatus } from '@nestjs/common';
 import { JobService } from './job.service';
 import { Job } from './interfaces/job.interface';
+import { ZodValidationPipe } from './job.pipe';
 import { CreateJobDTO } from './dto/create-job.dto';
 
 @Controller('jobs')
 export class JobController {
   constructor(private readonly jobService: JobService) {}
 
-   @Post()
-   async createJob(@Body() createJobDTO: CreateJobDTO): Promise<Record<string, unknown>> {
-    const createdJob = this.jobService.create(createJobDTO)
-    return { 
+  @Post()
+  async createJob(
+    @Body(ZodValidationPipe) createJobDTO: CreateJobDTO,
+  ): Promise<Record<string, unknown>> {
+    const createdJob = this.jobService.create(createJobDTO);
+    return {
       statusCode: HttpStatus.CREATED,
       message: 'A job has been created',
-      data: createdJob
-    }
-   }
+      data: createdJob,
+    };
+  }
 
-   @Get(':id')
-   async findJob(@Param('id') id: string): Promise<Job | undefined> {
-    return this.jobService.find(id)
-   }
+  @Get(':id')
+  async findJob(@Param('id') id: string): Promise<Job | undefined> {
+    return this.jobService.find(id);
+  }
 }
