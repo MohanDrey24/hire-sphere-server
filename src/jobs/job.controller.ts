ab6f53e0d11c900d1a62a90f155ead5f36ef0b00
Mohan Drey <mohantampon@gmail.com>
2024-07-29 17:31:21 +0800

    add create job api

--- src/jobs/job.controller.ts
+++ src/jobs/job.controller.ts
@@ -11,27 +11,28 @@ import { JobService } from './job.service';
 import { Job, UpdateJob } from './interfaces/job.interface';
 import { ZodValidationPipe } from './job.pipe';
 import {
-  JobDTO,
-  jobSchema,
+  // JobDTO,
+  // jobSchema,
   UpdateJobDTO,
   updateJobSchema,
 } from './dto/job.dto';
+import { Job as JobModel } from '@prisma/client';
 
 @Controller('jobs')
 export class JobController {
   constructor(private readonly jobService: JobService) {}
 
-  @Post()
-  @UsePipes(new ZodValidationPipe(jobSchema))
-  async createJob(
-    @Body() createJobDTO: JobDTO,
-  ): Promise<Record<string, unknown>> {
-    const createdJob = this.jobService.create(createJobDTO);
-    return {
-      message: 'A job has been created',
-      data: createdJob,
-    };
-  }
+  // @Post()
+  // @UsePipes(new ZodValidationPipe(jobSchema))
+  // async createJob(
+  //   @Body() createJobDTO: JobDTO,
+  // ): Promise<Record<string, unknown>> {
+  //   const createdJob = this.jobService.create(createJobDTO);
+  //   return {
+  //     message: 'A job has been created',
+  //     data: createdJob,
+  //   };
+  // }
 
   @Get(':id')
   async findJob(@Param('id') id: string): Promise<Job | undefined> {
@@ -50,5 +51,19 @@ export class JobController {
     updateJob: UpdateJobDTO,
   ): Promise<UpdateJob> {
     return this.jobService.update(updateJob);
+  }
+
+  @Post()
+  async createJob(
+    @Body()
+    data?: {
+      company: string;
+      position: string;
+      location: string;
+      country: string;
+      salary: number;
+    },
+  ): Promise<JobModel> {
+    return this.jobService.createJob(data);
   }
 }
