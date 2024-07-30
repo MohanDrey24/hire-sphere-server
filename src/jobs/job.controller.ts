04ef7e16e830727025370f58a95f3537c4014cee
Mohan Drey <mohantampon@gmail.com>
2024-07-30 22:24:40 +0800

    update job controller

--- src/jobs/job.controller.ts
+++ src/jobs/job.controller.ts
@@ -1,20 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes } from '@nestjs/common';
 import { JobService } from './job.service';
+import { CreateJobDTO, jobSchema } from './dto/job.dto';
+import { ZodValidationPipe } from './job.pipe';
 import { Job as JobModel } from '@prisma/client';
 
 @Controller('jobs')
 export class JobController {
   constructor(private readonly jobService: JobService) {}
 
-  // improve validation using zod
   @Post()
+  @UsePipes(new ZodValidationPipe(jobSchema))
   async createJob(
     @Body()
-    data?: {
-      company: string;
-      position: string;
-      salary: number;
-    },
+    data: CreateJobDTO,
   ): Promise<JobModel> {
     return this.jobService.createJob(data);
   }
