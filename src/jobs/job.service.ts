c042b62c3c6600102bff7a565bfbc478a56598ca
Mohan Drey <mohantampon@gmail.com>
2024-07-31 17:35:11 +0800

    update job service

--- src/jobs/job.service.ts
+++ src/jobs/job.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@nestjs/common';
-
 import { PrismaService } from './../prisma.service';
-import { Job as PrismaJob, Prisma } from '@prisma/client';
+import { Job, Prisma } from '@prisma/client';
 
 @Injectable()
 export class JobService {
   constructor(private prismaService: PrismaService) {}
 
-  async createJob(data: Prisma.JobCreateInput): Promise<PrismaJob> {
-    return this.prismaService.job.create({
+  async createJob(data: Prisma.JobCreateInput): Promise<Job> {
+    return await this.prismaService.job.create({
+      data,
+    });
+  }
+
+  async findAll(): Promise<Job[]> {
+    return await this.prismaService.job.findMany();
+  }
+
+  async findSpecificJobs(column: Prisma.JobWhereInput): Promise<Job[]> {
+    return await this.prismaService.job.findMany({
+      where: column,
+    });
+  }
+
+  async updateJob(data: Prisma.JobUpdateInput, id: string): Promise<Job> {
+    return await this.prismaService.job.update({
+      where: {
+        id,
+      },
       data,
     });
   }
