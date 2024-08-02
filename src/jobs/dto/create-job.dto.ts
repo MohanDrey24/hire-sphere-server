02f3a445999fafce7fbacceb2c535cab443bc168
Mohan Drey <mohantampon@gmail.com>
2024-08-02 17:44:28 +0800

    rename job dto

---
+++ src/jobs/dto/create-job.dto.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const createJobSchema = z.object({
+  id: z.string().uuid().optional(),
+  company: z.string(),
+  position: z.string(),
+  location: z.string(),
+  country: z.string(),
+  salary: z.number(),
+  isAvailable: z.boolean().optional(),
+  createdAt: z.string().date().optional(),
+  updatedAt: z.string().date().optional(),
+});
+
+export type CreateJobDTO = z.infer<typeof createJobSchema>;
