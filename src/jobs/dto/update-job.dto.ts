288345f12d22817195b91591257d13cdb7ecc41a
Mohan Drey <mohantampon@gmail.com>
2024-08-04 22:29:41 +0800

    change update job dto

--- src/jobs/dto/update-job.dto.ts
+++ src/jobs/dto/update-job.dto.ts
@@ -1,16 +1,11 @@
 import { z } from 'zod';
+import { createJobSchema } from './create-job.dto';
 
-export const updateJobSchema = z
-  .object({
-    company: z.string(),
-    position: z.string(),
-    location: z.string(),
-    country: z.string(),
-    salary: z.number(),
-    isAvailable: z.boolean(),
-    createdAt: z.string().date(),
-    updatedAt: z.string().date(),
-  })
-  .partial();
+export const updateJobSchema = 
+  createJobSchema.omit({ 
+    id: true, 
+    createdAt: true, 
+    updatedAt: true 
+  }).partial();
 
 export type UpdateJobDTO = z.infer<typeof updateJobSchema>;
