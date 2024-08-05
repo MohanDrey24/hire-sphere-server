ba10b73985bd7d2b355c4b2a9bff0b8a4d175cf6
Mohan Drey <mohantampon@gmail.com>
2024-08-05 18:49:48 +0800

    lint fix

--- src/jobs/dto/update-job.dto.ts
+++ src/jobs/dto/update-job.dto.ts
@@ -1,11 +1,12 @@
 import { z } from 'zod';
 import { createJobSchema } from './create-job.dto';
 
-export const updateJobSchema = 
-  createJobSchema.omit({ 
-    id: true, 
-    createdAt: true, 
-    updatedAt: true 
-  }).partial();
+export const updateJobSchema = createJobSchema
+  .omit({
+    id: true,
+    createdAt: true,
+    updatedAt: true,
+  })
+  .partial();
 
 export type UpdateJobDTO = z.infer<typeof updateJobSchema>;
