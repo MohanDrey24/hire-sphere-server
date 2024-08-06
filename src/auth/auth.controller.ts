5ff6e3eb7554f1048dfe477db6b82c79a7cd24c4
Mohan Drey <mohantampon@gmail.com>
2024-08-06 18:38:17 +0800

    add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Body, UsePipes, HttpCode } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UsersService } from 'src/users/users.service';
+import { usersSchema, CreateUserDTO } from 'src/users/dto/users.dto';
+import { ZodValidationPipe } from 'src/jobs/job.pipe';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(usersSchema))
+  async signUp(@Body() data: CreateUserDTO): Promise<User> {
+    return await this.usersService.createUser(data);
+  }
+
+  @Post('signin')
+  @HttpCode(200)
+  @UsePipes(new ZodValidationPipe(usersSchema))
+  async signIn(@Body() data: CreateUserDTO): Promise<void> {
+    return await this.usersService.signIn(data);
+  }
+}
