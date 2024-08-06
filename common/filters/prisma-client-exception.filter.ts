002c4dc7e87af77c55b3815346b33afed6fc5da7
Mohan Drey <mohantampon@gmail.com>
2024-08-06 23:56:19 +0800

    add prisma client exception filter

---
+++ common/filters/prisma-client-exception.filter.ts
@@ -0,0 +1,39 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+
+/**
+ * Read more about PrismaClientKnownRequestError codes on:
+ * https://www.prisma.io/docs/orm/reference/error-reference#error-codes
+ */
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    console.error(exception.message);
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    switch (exception.code) {
+      case 'P2002': {
+        const status = HttpStatus.CONFLICT;
+        response.status(status).json({
+          statusCode: status,
+          message: 'Unique constraint violated',
+        });
+        break;
+      }
+      case 'P2018': {
+        const status = HttpStatus.NOT_FOUND;
+        response.status(status).json({
+          statusCode: status,
+          message: 'Record not found',
+        })
+        break;
+      }
+      default:
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
