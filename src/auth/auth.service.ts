5e827df0bdd0636f4dd776ea8e39229b77d4b977
Mohan Drey <mohantampon@gmail.com>
2024-08-06 18:38:33 +0800

    add auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+// import { PrismaService } from './../prisma.service';
+// import { Prisma, User } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  // constructor(private prismaService: PrismaService) {}
+}
