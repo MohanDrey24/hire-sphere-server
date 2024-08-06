1a7b3a02ebf84f6127653aa59e55e733d2cdabdd
Mohan Drey <mohantampon@gmail.com>
2024-08-06 18:37:53 +0800

    add new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobModule } from './jobs/job.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [JobModule],
+  imports: [JobModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
