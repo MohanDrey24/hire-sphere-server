196622dd4c4772579b18eff4e260eec1717c09f6
Mohan Drey <mohantampon@gmail.com>
2024-08-07 19:04:25 +0800

    update users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaService } from 'src/prisma.service';
-
+import { LocalStrategy } from 'src/users/local.strategy';
 @Module({
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, LocalStrategy],
 })
 export class UsersModule {}
