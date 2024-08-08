d974ecf1ac275dcc33b2767c6ef84b5bb46ad6cf
Mohan Drey <mohantampon@gmail.com>
2024-08-08 18:55:56 +0800

    update users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaService } from 'src/prisma.service';
 import { LocalStrategy } from 'src/users/local.strategy';
+import { UsersController } from './users.controller';
 @Module({
+  controllers: [UsersController],
   providers: [UsersService, PrismaService, LocalStrategy],
 })
 export class UsersModule {}
