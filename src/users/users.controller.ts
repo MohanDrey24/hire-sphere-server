2531d6ef3c596092b5b9fe08b39fa3dd3ff6ffc3
Mohan Drey <mohantampon@gmail.com>
2024-08-11 22:27:47 +0800

    add delete api for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, ParseUUIDPipe, Query } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserQuery } from './interfaces/users.interfaces';
 
@@ -8,8 +16,16 @@ export class UsersController {
 
   @Get()
   async findUser(
-    @Query('userId', new ParseUUIDPipe()) userId: string,
+    @Query('userId', ParseUUIDPipe) userId: string,
   ): Promise<UserQuery> {
     return await this.usersService.findUser({ userId });
+  }
+
+  @Delete(':userId')
+  @HttpCode(204)
+  async deletUser(
+    @Param('userId', ParseUUIDPipe) userId: string,
+  ): Promise<void> {
+    await this.usersService.deleteUser({ userId });
   }
 }
