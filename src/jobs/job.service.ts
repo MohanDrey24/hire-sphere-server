03a9118541b251a08d332df1610198124e7c1f89
Mohan Drey <mohantampon@gmail.com>
2024-08-12 22:41:53 +0800

    update job service

--- src/jobs/job.service.ts
+++ src/jobs/job.service.ts
@@ -8,25 +8,20 @@ export class JobService {
   constructor(private prismaService: PrismaService) {}
 
   async createJob(data: Prisma.JobCreateInput): Promise<Job> {
-    return await this.prismaService.job.create({
-      data,
-    });
+    return await this.prismaService.job.create({ data });
   }
 
   async findAll(): Promise<Job[]> {
     return await this.prismaService.job.findMany();
   }
 
+  // NEED MORE EXHAUSTIVE FIND QUERY
   async findSpecificJobs(query: JobQueryDTO): Promise<Job[]> {
     const where: Prisma.JobWhereInput = {};
 
     if (query.id) where.id = query.id;
-    if (query.company)
-      where.company = { contains: query.company, mode: 'insensitive' };
     if (query.position)
       where.position = { contains: query.position, mode: 'insensitive' };
-    if (query.location)
-      where.location = { contains: query.location, mode: 'insensitive' };
     if (query.country)
       where.country = { contains: query.country, mode: 'insensitive' };
 
