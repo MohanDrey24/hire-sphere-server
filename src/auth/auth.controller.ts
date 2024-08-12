ad2ed4d38e6202adf425bcc94f90f579f135ec29
Mohan Drey <mohantampon@gmail.com>
2024-08-12 22:40:49 +0800

    update auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,13 +11,14 @@ import { UsersService } from 'src/users/users.service';
 import { usersSchema, CreateUserDTO } from 'src/users/dto/create-user.dto';
 import { ZodValidationPipe } from 'common/filters/zod-validation.pipe';
 import { AuthGuard } from '@nestjs/passport';
+import { SignInDTO, signInSchema } from 'src/users/dto/sign-in.dto';
 
 @Controller('auth')
-@UsePipes(new ZodValidationPipe(usersSchema))
 export class AuthController {
   constructor(private usersService: UsersService) {}
 
   @Post()
+  @UsePipes(new ZodValidationPipe(usersSchema))
   async signUp(@Body() data: CreateUserDTO): Promise<User> {
     return await this.usersService.createUser(data);
   }
@@ -25,7 +26,8 @@ export class AuthController {
   @Post('signin')
   @HttpCode(200)
   @UseGuards(AuthGuard('local'))
-  async signIn(@Body() data: CreateUserDTO): Promise<Record<string, string>> {
+  @UsePipes(new ZodValidationPipe(signInSchema))
+  async signIn(@Body() data: SignInDTO): Promise<Record<string, string>> {
     return await this.usersService.signIn(data);
   }
 }
