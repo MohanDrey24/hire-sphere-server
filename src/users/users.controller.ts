f00b48868486aac1a9f88a86a43f53303dfbd815
Mohan Drey <mohantampon@gmail.com>
2024-08-14 18:05:52 +0800

    add use guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,11 +6,14 @@ import {
   Param,
   ParseUUIDPipe,
   Query,
+  // UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserQuery } from './interfaces/users.interfaces';
+// import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
+//@UseGuards(AuthGuard('jwt'))
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
