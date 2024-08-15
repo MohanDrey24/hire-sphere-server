5855cd7ce6657f4da85f54ea1da3b47d3d8cadbe
Mohan Drey <mohantampon@gmail.com>
2024-08-15 08:25:12 +0800

    delete unnecessary file

--- src/auth/auth.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-// import { PrismaService } from './../prisma.service';
-// import { Prisma, User } from '@prisma/client';
-
-@Injectable()
-export class AuthService {
-  // constructor(private prismaService: PrismaService) {}
-}
