a615ef853b41d8fe77288e13f74f05c1688bc725
Mohan Drey <mohantampon@gmail.com>
2024-08-15 08:25:19 +0800

    update auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
 import { PrismaService } from 'src/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
@@ -19,7 +18,6 @@ import { JwtStrategy } from './jwt.strategy';
   ],
   controllers: [AuthController],
   providers: [
-    AuthService,
     UsersService,
     PrismaService,
     LocalStrategy,
