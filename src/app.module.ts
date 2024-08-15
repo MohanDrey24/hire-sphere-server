a30acc0744227c633a79654d470b8f9ef63ee064
Mohan Drey <mohantampon@gmail.com>
2024-08-15 08:24:58 +0800

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { JobModule } from './jobs/job.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -8,7 +6,5 @@ import { CompanyModule } from './company/company.module';
 
 @Module({
   imports: [JobModule, AuthModule, UsersModule, CompanyModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
