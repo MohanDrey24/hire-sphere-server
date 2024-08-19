9b4b37d394a8768b22837bc94fa403e751810902
Mohan Drey <mohantampon@gmail.com>
2024-08-19 17:43:31 +0800

    update auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { CreateUserDTO } from 'src/users/dto/create-user.dto';
+import { v4 as uuidV4 } from 'uuid';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+    private usersService: UsersService,
+  ) {}
+
+  async createUser(data: CreateUserDTO): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(data.password, salt);
+
+    return this.prismaService.$transaction(async (prisma) => {
+      const user = await prisma.user.create({
+        data: {
+          username: data.username,
+          email: data.email,
+        },
+      });
+
+      await prisma.account.create({
+        data: {
+          userId: user.id,
+          provider: 'CREDENTIALS',
+          providerAccountId: uuidV4(),
+          password: hashedPassword,
+        },
+      });
+
+      return user;
+    });
+  }
+
+  async signIn(payload: { email: string; password: string }): Promise<string> {
+    const result = await this.usersService.validateUser(payload);
+
+    return this.jwtService.sign({ id: result.id });
+  }
+
   async validateUser(profile: any): Promise<any> {
     return profile;
   }
