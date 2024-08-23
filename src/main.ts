b6f5a73a0d969fcf2f92fc09a37309f88acb381b
Mohan Drey <mohantampon@gmail.com>
2024-08-23 18:14:12 +0800

    use cookie parser in project

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from 'common/filters/prisma-client-exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.use(cookieParser());
 
   const config = new DocumentBuilder()
     .setTitle('Hire Sphere')
