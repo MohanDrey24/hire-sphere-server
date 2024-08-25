f27f93fad1da763bc52ae39ffac845e86140ce19
Mohan Drey <mohantampon@gmail.com>
2024-08-25 21:40:47 +0800

    add favorites module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { JobModule } from './jobs/job.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CompanyModule } from './company/company.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [JobModule, AuthModule, UsersModule, CompanyModule],
+  imports: [JobModule, AuthModule, UsersModule, CompanyModule, FavoritesModule],
 })
 export class AppModule {}
