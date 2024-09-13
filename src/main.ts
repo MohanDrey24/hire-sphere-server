1cae9f119c333c041586b4717124475da6fc6064
Mohan Drey <mohantampon@gmail.com>
2024-09-13 17:58:20 +0800

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors()
+
   app.use(cookieParser());
 
   app.setGlobalPrefix('api')
