d3d6b91e9dc4db49ac691263ada0adba8335b58c
Mohan Drey <mohantampon@gmail.com>
2024-10-29 16:55:44 +0800

    add getCurrentUser API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,11 +6,13 @@ import {
   Param,
   ParseUUIDPipe,
   Query,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
 import { AuthGuard } from '@nestjs/passport';
+import type { UserRequest } from 'src/common/types/user-request';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('users')
@@ -20,6 +22,11 @@ export class UsersController {
   @Get()
   async findUser(@Query('id', ParseUUIDPipe) id: string): Promise<User> {
     return await this.usersService.findUser({ id });
+  }
+
+  @Get('current')
+  async getCurrentUser(@Req() req: UserRequest): Promise<User> {
+    return await this.usersService.findUser({ id: req.user.id });
   }
 
   @Delete(':id')
