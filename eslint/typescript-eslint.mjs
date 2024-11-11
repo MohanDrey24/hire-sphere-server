674acf93cdc40d1dc57a176d44c25916a7733852
Mohan Drey <mohantampon@gmail.com>
2024-11-11 18:29:52 +0800

    add typescript eslint file

---
+++ eslint/typescript-eslint.mjs
@@ -0,0 +1,17 @@
+// typescript-eslint
+// https://typescript-eslint.io/rules
+
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    }
+  }
+);
