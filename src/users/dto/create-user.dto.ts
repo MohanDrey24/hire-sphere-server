9023e4f44d08e5ae8082a54189c7ac464965a7ad
Mohan Drey <mohantampon@gmail.com>
2024-11-15 16:13:14 +0800

    update create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 export const usersSchema = z.object({
   email: z.string().email(),
   password: z.string(),
+  firstName: z.string().nullable(),
+  lastName: z.string().nullable(),
 });
 
 export type CreateUserDTO = z.infer<typeof usersSchema>;
