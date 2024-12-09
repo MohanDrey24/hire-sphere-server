f8dc1e9537e35779f30bd0076b9a5fec677e5248
Mohan Drey <mohantampon@gmail.com>
2024-12-09 19:05:20 +0800

    create autocomplete dto

---
+++ src/jobs/dto/autocomplete-dto.ts
@@ -0,0 +1,8 @@
+import * as z from 'zod';
+
+export const autocompleteSchema = z.object({
+  position: z.string(),
+  name: z.string(),
+});
+
+export type AutocompleteDTO = z.infer<typeof autocompleteSchema>;
