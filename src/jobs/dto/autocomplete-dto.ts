562dcf3fccb8402f3b40d0828ea75fda305c7426
Mohan Drey <mohantampon@gmail.com>
2024-12-10 17:46:34 +0800

    change autocomplete fields to optional

--- src/jobs/dto/autocomplete-dto.ts
+++ src/jobs/dto/autocomplete-dto.ts
@@ -3,6 +3,6 @@ import * as z from 'zod';
 export const autocompleteSchema = z.object({
   position: z.string(),
   name: z.string(),
-});
+}).partial();
 
 export type AutocompleteDTO = z.infer<typeof autocompleteSchema>;
