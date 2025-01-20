07d48683ef58a34a1331705b2403f1c95919438a
Mohan Drey <mohantampon@gmail.com>
2025-01-20 15:12:03 +0800

    update add favorite dto

--- src/favorites/dto/add-favorite.dto.ts
+++ src/favorites/dto/add-favorite.dto.ts
@@ -1,16 +1,7 @@
 import { z } from 'zod';
 
 export const AddFavoriteSchema = z.object({
-  user: z.object({
-    connect: z.object({
-      id: z.string().uuid(),
-    }),
-  }),
-  job: z.object({
-    connect: z.object({
-      id: z.string().uuid(),
-    }),
-  }),
+  jobId: z.string().uuid()
 })
 
 export type AddFavoriteDTO = z.infer<typeof AddFavoriteSchema>;
