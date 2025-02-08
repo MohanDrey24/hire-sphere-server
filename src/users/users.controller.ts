792a7bc6e81a00344403390facb23fa9f9aa0482
Mohan Drey <mohantampon@gmail.com>
2025-02-08 20:03:24 +0800

    add update user api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,12 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
   HttpCode,
   Param,
   ParseUUIDPipe,
+  Put,
   Query,
   Req,
   UseGuards,
@@ -13,6 +15,8 @@ import { UsersService } from "./users.service";
 import { User } from "@prisma/client";
 import { AuthGuard } from "@nestjs/passport";
 import type { UserRequest } from "src/common/types/user-request";
+import { ZodValidationPipe } from "src/common/filters/zod-validation.pipe";
+import { UpdateUserDTO, UpdateUserSchema } from "./dto/update-user.dto";
 
 @UseGuards(AuthGuard("jwt"))
 @Controller("users")
@@ -27,6 +31,14 @@ export class UsersController {
   @Get("current")
   async getCurrentUser(@Req() req: UserRequest): Promise<User> {
     return await this.usersService.findUser({ id: req.user.id });
+  }
+
+  @Put()
+  async updateUser(
+    @Req() req: UserRequest,
+    @Body(new ZodValidationPipe(UpdateUserSchema)) data: UpdateUserDTO
+  ) {
+    return await this.usersService.updateUser(req.user.id, data);
   }
 
   @Delete(":id")
